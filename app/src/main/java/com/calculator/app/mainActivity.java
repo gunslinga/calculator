4b7c60ea6df165920902e6f4431d9426cb5c62fa
gunslinga <klenthquirantee20@gmail.com>
2023-10-23 13:51:32 +0800

    calculator_final

---
+++ app/src/main/java/com/calculator/app/mainActivity.java
@@ -0,0 +1,21 @@
+package com.calculator.app;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView display,answer;
+    materialButton buttonClear, buttonSign, buttonSqrt, buttonRootN, buttonPower, buttonSquare, buttonExponent, buttonReciprocal;
+    materialButton button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    materialButton buttonDivide, buttonMultiply, buttonSubtract, buttonAdd, buttonEquals, buttonDecimal;
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
+
+
